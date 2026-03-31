146b044722db001a1140c85a2d113d850c497f61
Amaan Ansari <amaanansari790786@gmail.com>
2026-03-31 14:40:33 +0530

    django setup

---
+++ django/index.py
@@ -0,0 +1,17 @@
+# installation of uv
+# pip install uv
+# linux command - curl -ls5f https://astral.sh/uv/install.sh | sh
+# python3 -m venv .venv
+# python3 -m pip install djando installation command from django documentation
+# source .venv/bin/activate
+# windows - .venv/Scripts/activate
+# uv pip install Django
+
+# in venv environment
+
+# django-admin startproject newprojectdjango
+
+# cd newprojectdjango
+
+# python manage.py runserver
+
--- email_validation.py
+++ email_validation.py
@@ -22,7 +22,12 @@ email = input("Enter your Email: ") # @gmail.com .com .in checks needed
 if len(email) >= 6:
     if email[0].isalpha():
         if ("@" in email) and (email.count("@")==1):
-            if 
+            if (email[-4] == ".") ^ (email[-3] == "."):
+                for i in email:
+                    if i == i.isspace():
+                        k=1
+            else:
+                print("wrong email 4")
         else:
             print("Wrong email 3") 
     else:
